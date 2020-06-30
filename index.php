162a22c0e6b9210d684a2fdbe0b74f518a29d13c
fuliang <fuliang19890809@163.com>
2020-06-30 14:58:46 +0800

    提交

--- index.php
+++ index.php
@@ -1,5 +1,12 @@
 <?php
 $m = new MongoClient(); // 连接默认主机和端口为：mongodb://localhost:27017
 $db = $m->test; // 获取名称为 "test" 的数据库
-$collection = $db->createCollection("runoob");
-echo "集合创建成功";
+$collection = $db->runoob1; // 选择集合
+// 更新文档
+$collection->update(array("title"=>"MongoDB1"), array('$set'=>array("title"=>"MongoDB 教程")));
+// 显示更新后的文档
+$cursor = $collection->find();
+// 循环显示文档标题
+foreach ($cursor as $document) {
+    var_dump($document);
+}
